9a7f0358a4cdc0fcba6e43a453961d35d1e23a69
printMelen <alvaro.redondofgb@gmail.com>
2023-12-19 14:30:54 +0100

    Section repetidos a la mitad

--- index.php
+++ index.php
@@ -51,23 +51,23 @@
                     </div>
                </div>
           </section>
-          <section class="flex justify-between gap-[100px]">
+          <section class="container max-w-screen-xl mx-auto flex justify-between gap-[100px] ">
                <div class="my-auto">
-                    <h2 class="">Automated Reports & Widget Alerts</h2>
+                    <h2 class="text-letraNegra text-xm font-bold">Automated Reports & Widget Alerts</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
                <img src="assets\images\screen-01.svg" alt="" srcset="">
           </section>
-          <section class="flex justify-between gap-[100px]">
+          <section class="container max-w-screen-xl mx-auto flex justify-between gap-[100px]">
                <img src="assets\images\screen-02.svg" alt="" srcset="">
                <div class="my-auto">
-                    <h2>Fully customizable to address your needs</h2>
+                    <h2 class="text-letraNegra text-xm font-bold">Fully customizable to address your needs</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
           </section>
-          <section class="flex justify-between gap-[100px]">
+          <section class="container max-w-screen-xl mx-auto flex justify-between gap-[100px]">
                <div class="my-auto">
-                    <h2>Pre-built Dashboard Templates</h2>
+                    <h2 class="text-letraNegra text-xm font-bold">Pre-built Dashboard Templates</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
                <img src="assets\images\screen-03.svg" alt="" srcset="">
