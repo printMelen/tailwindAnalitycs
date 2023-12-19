f619edc245ecb9ed38dbcd928911fca18e294fc2
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2023-12-19 16:44:13 +0100

    Cambio de imagenes, sombra y texto del section precios

--- index.php
+++ index.php
@@ -56,10 +56,10 @@
                     <h2 class="text-letraNegra text-xm font-bold">Automated Reports & Widget Alerts</h2>
                     <p class="text-letraHeader leading-parrafo font-normal text-base mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
-               <img src="assets\images\screen-01.svg" alt="" srcset="">
+               <img src="assets\images\screen-01Shadowless.svg" alt="" srcset="" class="drop-shadow-xl">
           </section>
           <section class="container max-w-screen-xl mx-auto flex justify-between gap-[100px] my-[120px]">
-               <img src="assets\images\screen-02.svg" alt="" srcset="">
+               <img src="assets\images\screen-02Shadowless.svg" alt="" srcset="" class="drop-shadow-xl">
                <div class="my-auto">
                     <h2 class="text-letraNegra text-xm font-bold">Fully customizable to address your needs</h2>
                     <p class="text-letraHeader leading-parrafo font-normal text-base mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
@@ -70,11 +70,11 @@
                     <h2 class="text-letraNegra text-xm font-bold">Pre-built Dashboard Templates</h2>
                     <p class="text-letraHeader leading-parrafo font-normal text-base mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.</p>
                </div>
-               <img src="assets\images\screen-03.svg" alt="" srcset="">
+               <img src="assets\images\screen-03Shadowless.svg" alt="" srcset="" class="drop-shadow-xl">
           </section>
           <section class="text-center">
-               <h2>Pricing Plans</h2>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. </p>
+               <h2 class="text-white text-xm font-bold mb-[30px]">Pricing Plans</h2>
+               <p class="text-letraHeader leading-parrafo font-normal text-base w-[1060px] mx-auto mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet <br> volutpat pellentesque volutpat est. </p>
                <div class="flex justify-center gap-[80px]">
                     <div class="flex flex-col justify-center
                     w-[340px] h-[440px]
