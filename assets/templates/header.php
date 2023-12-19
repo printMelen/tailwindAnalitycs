1fb4a2eeeb5918986101287d23dd92256372c63f
printMelen <alvaro.redondofgb@gmail.com>
2023-12-19 13:10:06 +0100

    header acabado

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,9 +1,11 @@
-<header class="container max-w-screen-xl mx-auto bg-transparent grid 
-grid-cols-2 justify-between content-center py-14 
+<header class="container max-w-screen-xl mx-auto bg-transparent 
+grid grid-cols-[139px_352px_minmax(234px,_1fr)] pt-[50px]
+items-center
+text-letraHeader
 ">
-    <img src="assets\images\logo-footer.png" alt="">
-    <nav>
-        <ul>
+    <img src="assets\images\logo-footer.png" alt="" class="">
+    <nav class="pl-[89px]">
+        <ul class="flex gap-[36px]">
             <li><a href="#">Products</a></li>
             <li><a href="#">Pricing</a></li>
             <li><a href="#">FAQ</a></li>
@@ -11,9 +13,9 @@ grid-cols-2 justify-between content-center py-14
             <li><a href="#">Blog</a></li>
         </ul>
     </nav>
-    <div>
-        <button class="no-border">Sign in</button>
-        <button>Sign Up</button>
+    <div class=" flex justify-end">
+        <button class="no-border mr-[40px]">Sign in</button>
+        <button class="border border-bordes py-[10px] px-[53px] rounded-full">Sign Up</button>
     </div>
 </header>
 
